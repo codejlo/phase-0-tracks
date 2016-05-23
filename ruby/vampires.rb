1a9a154591073c22fd11dff6e6ed16ae65566236
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-05-23 16:32:13 -0700

    Added ending phrase and rewrote conditionals to include more efficient if/elsif/else logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,7 +6,7 @@ employees = gets.chomp().to_i
 
 	# initialize result with default value
 
-	result = "Results inconclusive."
+	result = nil
 
 	# Asking questions of potential vampire
 
@@ -25,37 +25,45 @@ employees = gets.chomp().to_i
 	puts "Would you like to enroll in the company’s health insurance? (y/n)"
 	insurance = gets.chomp()
 
-	# Test for a vampire on first set of questions
 
-	if (age == 2016-year) && (garlic == "y" || insurance == "y")
-		result = "Probably not a vampire."
-	end
+	# Test for a vampire on the remainder of questions, in order of specificity
+	# Only execute tests if they are necessary
 
-	# Loop over allergies to screen for sunshine
-	puts "Please enter any allergies. Press enter after each. Type 'done' when finished"
-	allergies = nil
-	until allergies == "sunshine" || allergies == "done"
-		allergies = gets.chomp()
-		if allergies == "sunshine"
-			result = "Probably a vampire."
+	if (name == "Drake Cula") || (name == "Tu Fang")
+		result = "Definitely a vampire."
+	elsif (age != 2016-year) && (garlic == "n") && (insurance == "n")
+		result = "Almost certainly a vampire."
+	elsif (age != 2016-year) && (garlic == "n" || insurance == "n")
+		result = "Probably a vampire."
+	else
+		# Loop over allergies to screen for sunshine; test only relevant if more specific tests fail.
+		# Placing the loop here is efficient because it only runs if it is useful
+		#(i.e. another more specific or equivalent test already hasnt been run)
+		
+		puts "Please enter any allergies. Press enter after each. Type 'done' when finished"
+		allergies = nil
+		until (allergies == "sunshine") || (allergies == "done")
+			allergies = gets.chomp()
+			if allergies == "sunshine"
+				result = "Probably a vampire."
+			end
 		end
 	end
 
-	if !(age == 2016-year) && (garlic == "n" || insurance == "n")
-		result = "Probably a vampire."
-	end
-
-	if !(age == 2016-year) && garlic == "n" && insurance == "n"
-		result = "Almost certainly a vampire."
-	end
-
-	if name == "Drake Cula" || name == "Tu Fang"
-		result = "Definitely a vampire."
+	# If they have not tested positive for being a vampire, then assign result.
+	if result == nil
+		if (age == 2016-year) && (garlic == "y" || insurance == "y")
+			result = "Probably not a vampire."
+		else
+			result = "Results inconclusive."
+		end
 	end
 
 
 	# Print result
 
 	puts result
+
+	puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 end
