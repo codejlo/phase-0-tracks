54f7c2df2d3d0c3a54114d79d5ff995435ee1234
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-05-23 15:58:49 -0700

    Adding loop logic and allergy test

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,36 +1,61 @@
-# Asking questions of potential vampire
+# Determine how many interviewees
+puts "How many employees will be processed?"
+employees = gets.chomp().to_i
 
-puts "What is your name"
-name = gets.chomp()
+(1..employees).each do |i|
 
-puts "How old are you?"
-age = gets.chomp().to_i
+	# initialize result with default value
 
-puts "What year were you born?"
-year = gets.chomp().to_i
+	result = "Results inconclusive."
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic = gets.chomp()
+	# Asking questions of potential vampire
 
-puts "Would you like to enroll in the company’s health insurance? (y/n)"
-insurance = gets.chomp()
+	puts "What is your name"
+	name = gets.chomp()
 
-result = "Results inconclusive."
+	puts "How old are you?"
+	age = gets.chomp().to_i
 
-if (age == 2016-year) && (garlic == "y" || insurance == "y")
-	result = "Probably not a vampire."
+	puts "What year were you born?"
+	year = gets.chomp().to_i
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	garlic = gets.chomp()
+
+	puts "Would you like to enroll in the company’s health insurance? (y/n)"
+	insurance = gets.chomp()
+
+	# Test for a vampire on first set of questions
+
+	if (age == 2016-year) && (garlic == "y" || insurance == "y")
+		result = "Probably not a vampire."
+	end
+
+	# Loop over allergies to screen for sunshine
+	puts "Please enter any allergies. Press enter after each. Type 'done' when finished"
+	allergies = nil
+	until allergies == "sunshine" || allergies == "done"
+		allergies = gets.chomp()
+		if allergies == "sunshine"
+			result = "Probably a vampire."
+		end
+	end
+
+	if !(age == 2016-year) && (garlic == "n" || insurance == "n")
+		result = "Probably a vampire."
+	end
+
+	if !(age == 2016-year) && garlic == "n" && insurance == "n"
+		result = "Almost certainly a vampire."
+	end
+
+	if name == "Drake Cula" || name == "Tu Fang"
+		result = "Definitely a vampire."
+	end
+
+
+	# Print result
+
+	puts result
+
 end
-
-if !(age == 2016-year) && (garlic == "n" || insurance == "n")
-	result = "Probably a vampire."
-end
-
-if !(age == 2016-year) && garlic == "n" && insurance == "n"
-	result = "Almost certainly a vampire."
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-	result = "Definitely a vampire."
-end
-
-puts result
