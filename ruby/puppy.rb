46ca5d5e3bc8f6a84b64cd337d6ac315added4f8
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-06-06 12:58:27 -0700

    Added comments for a new class "Puppy"

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+# puppy.rb
+# Jason Lorentzen
+=begin
+
+ANIMAL ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Cuteness level: high
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Size: small
+
+BEHAVIOR -----------------------
+Bark
+Wag tail
+Roll over
+Play dead
+
+=end
+
