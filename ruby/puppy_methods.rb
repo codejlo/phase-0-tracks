a3ebd2f6117fe9721471f182e8a2083562d154d0
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-06-06 14:18:07 -0700

    completed release 0 driver code and method definitions

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(int)
+  	int.times { puts "Woof!"}
+  end
+
+  def roll_over()
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	human_years * 7
+  end
+
+  def supper_time(food_array)
+  	food_array.each { |item| puts "Now i'm eating #{item}."}
+  end
+
 end
 
+
+
+fido = Puppy.new()
+
+fido.fetch("ball")
+fido.speak(5)
+fido.roll_over()
+p fido.dog_years(10)
+fido.supper_time(["beef","kibbles","cat"])
