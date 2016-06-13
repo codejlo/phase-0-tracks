295598f97d76175fcdc764bdc197a426d6009562
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-06-12 23:00:58 -0700

    Adding mixins and test code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,46 @@
+#module Shout
+#
+#	def self.yell_angrily(words)
+#    	words + "!!!" + " :("
+#  	end
+#
+#  	def self.yell_happily(words)
+#    	words + "!!!" + " :)"
+#  	end
+#
+#end
+#
+#
+#puts Shout.yell_angrily('AHHHHH')
+#puts Shout.yell_happily('WEEEEE')
+
 module Shout
 
-	def self.yell_angrily(words)
+	def yell_angrily(words)
     	words + "!!!" + " :("
   	end
 
-  	def self.yell_happily(words)
+  	def yell_happily(words)
     	words + "!!!" + " :)"
   	end
 
 end
 
 
-puts Shout.yell_angrily('AHHHHH')
-puts Shout.yell_happily('WEEEEE')
+class Reality_tv_judge
+	include Shout
+end
+
+
+class Basketball_coach
+	include Shout
+end
+
+
+black_vee_neck = Reality_tv_judge.new
+puts black_vee_neck.yell_angrily("That was the worst thing I have ever heard")
+puts black_vee_neck.yell_happily("I am astounded")
+
+coach_k = Basketball_coach.new
+puts coach_k.yell_angrily("Are you blind?")
+puts coach_k.yell_happily("Nice pass")
